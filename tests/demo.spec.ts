09c165457c0b4b8d5222a384476e26969d59bfbc
MichaelAlabi <michaelalabi82@gmail.com>
2026-04-20 21:51:44 +0100

    fixed for cicd process

--- tests/demo.spec.ts
+++ tests/demo.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('testing categories visibility', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await expect(page.getByRole('heading', { name: 'Hats' })).toBeVisible();
   await expect(page.getByRole('heading', { name: 'Jackets' })).toBeVisible();
