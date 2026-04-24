fba81e8e089a964d71fa29cf224c12dda27507fe
MichaelAlabi <michaelalabi82@gmail.com>
2026-04-24 10:27:59 +0100

    refactoring

--- tests/demo.spec.ts
+++ tests/demo.spec.ts
@@ -1,11 +1,20 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
+import dotenv from "dotenv";
 
-test('testing categories visibility', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await expect(page.getByRole('heading', { name: 'Hats' })).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'Jackets' })).toBeVisible();
-  await expect(page.getByText('SneakersShop Now')).toBeVisible();
-  await expect(page.getByText('WomensShop Now')).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'Mens', exact: true })).toBeVisible();
-  console.log('All categories are visible as expected.');
+dotenv.config();
+
+const APP_URL = process.env.APP_URL || "http://localhost:3000";
+
+test("testing categories visibility", async ({ page }) => {
+  await page.goto(APP_URL);
+
+  await expect(page.getByRole("heading", { name: "Hats" })).toBeVisible();
+  await expect(page.getByRole("heading", { name: "Jackets" })).toBeVisible();
+  await expect(page.getByText("SneakersShop Now")).toBeVisible();
+  await expect(page.getByText("WomensShop Now")).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Mens", exact: true })
+  ).toBeVisible();
+
+  console.log("All categories are visible as expected.");
 });
