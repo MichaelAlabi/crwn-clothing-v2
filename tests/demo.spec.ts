39a6404c46b19fa0d68a8f71957f70ebb2b05f37
MichaelAlabi <michaelalabi82@gmail.com>
2026-05-10 08:36:34 +0100

    adding dockerfiles

--- tests/demo.spec.ts
+++ tests/demo.spec.ts
@@ -1,14 +1,11 @@
-import { test, expect } from '@playwright/test';
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '../.env.local') });
+import { test, expect } from "@playwright/test";
 
-test('testing categories visibility', async ({ page }) => {
-  await page.goto(process.env.APP_URL || 'http://localhost:3000');
-  await expect(page.getByRole('heading', { name: 'Hats' })).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'Jackets & Sweaters' })).toBeVisible();
-  await expect(page.getByText('SneakersShop Now')).toBeVisible();
-  await expect(page.getByText('WomensShop Now')).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'Mens', exact: true })).toBeVisible();
-  console.log('All categories are visible as expected.');
+test('test', async ({ page }) => {
+  await page.goto('/');
+  await page.getByRole('heading', { name: 'Hats' }).click();
+  await page.getByRole('heading', { name: 'Jackets' }).click();
+  await page.getByText('SneakersShop Now').click();
+  await page.getByText('WomensShop Now').click();
+  await page.getByRole('heading', { name: 'Mens', exact: true }).click();
+  console.log('test');
 });
